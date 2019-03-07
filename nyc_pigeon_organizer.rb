5a6560bb3395ea0dfbb9c8061d449a0ce8ff2112
Daniel Schneider <daschne8@gmail.com>
2019-03-07 00:16:39 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -31,7 +31,7 @@ def nyc_pigeon_organizer(data)
   data.each do |cgl,cgl_data|
     cgl_data.each do |sub_cgl, names|
       names.each do |name|
-        names_hash[name][cgl] << sub_cgl
+        names_hash[name][cgl] << sub_cgl.to_s
       end
     end
   end
